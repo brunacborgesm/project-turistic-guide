df908c814c77292eebeacf30dab27cf6f53ad14b
Bruna Borges <brunaborgesdev@gmail.com>
2024-10-15 13:02:30 -0300

    initial setup

---
+++ tourist-guide/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className="bg-boho h-screen">
+        <div className="p-20">
+          <h1 className="text-red-500 uppercase font-bold text-sm">sua próxima viagem:</h1>
+          <h2 className="font-bold text-2xl pt-2">Conheça Busan</h2>
+          <img src="src/assets/hero.svg" alt="imagem de busan" className="mt-8 rounded-lg" />
+        </div>
+    </div>
+  )
+}
+
+export default App
